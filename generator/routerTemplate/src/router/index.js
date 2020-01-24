631f5a136da21c28a4539ba2907e4932018788a0
Kehang <815432837@qq.com>
2020-01-24 08:26:00 +0800

    router文件更新

--- generator/routerTemplate/src/router/index.js
+++ generator/routerTemplate/src/router/index.js
@@ -1,24 +1,23 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 Vue.use(Router);
-const createRouter = function (path,title,{keepAlive=false,notHeader=false,isTab}) {
-    let meta = {
-        title
-    };
-    notHeader&&(meta.notHeader = notHeader);
-    keepAlive&&(meta.keepAlive = keepAlive);
-    isTab&&(meta.tabName = path);
-    return {
-        path:`/${path}`,
-        name:path,
-        component:() => import(`../views/${path}`),
-        meta
-    }
+const createRouter = function (pre,path,keepAlive,children) {
+  let routerItem = {
+    path:`${pre}${path}`,
+    name:path,
+    component:() => import(`../views/${path}`)
+  };
+  let meta = keepAlive?{keepAlive}:null;
+  meta ? routerItem.meta = meta : null;
+  children ? routerItem.children = children : null;
+  return routerItem;
 };
 export default new Router({
-    routes:[
-        createRouter('login','登录',{notHeader:true}),
-        createRouter('home','首页',{isTab:true}),
-        createRouter('mine','我的',{isTab:true})
-    ]
+  routes:[
+    createRouter('/','login'),
+    createRouter('/','tabPage',true,[
+      createRouter('/tabPage/','home'),
+      createRouter('/tabPage/','mine'),
+    ])
+  ]
 })
